c59dfc55b7aec2aa21d551e91ed51c05f51d437e
Felix Weiler <felix.weiler@weilerd.de>
2025-04-06 00:00:42 +0200

    touch test

--- godot/game/root.gd
+++ godot/game/root.gd
@@ -17,3 +17,26 @@ func _physics_process(_delta):
 
 	if Input.is_action_just_pressed("next_level"):
 		Global.win_level()
+	
+var touch_events = {}
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+			Global.is_touch  = true
+			#Global.ui.touch_interface.show()
+			
+			var touch_count_pre = touch_events.size()
+			
+			if event.pressed:
+				touch_events[event.index] = event
+			else:
+				if touch_events.has(event.index):
+					touch_events.erase(event.index)
+			
+			var touch_count_post = touch_events.size()
+			
+			##
+			if touch_count_pre <= 1 and touch_count_post > 1:
+				Input.action_press("right_click")
+			elif touch_count_pre > 1 and touch_count_post <= 1:
+				Input.action_release("right_click")	
